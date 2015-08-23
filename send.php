8b5e998e6d89fd427690f6b39ffea3e9ef78099f
GuschBur <guschbur@gmail.com>
2015-08-23 02:38:03 +0200

    Added Copyright claim

--- send.php
+++ send.php
@@ -14,10 +14,10 @@ $name = $_POST["name"];
 $email = $_POST["email"];
 $message = $_POST["message"];
 $text = "Name: $name<br>
-         Email: $email<br> 
+         Email: $email<br>
          Message: $message";
-$headers = "MIME-Version: 1.0" . "\r\n"; 
-$headers .= "Content-type:text/html; charset=utf-8" . "\r\n"; 
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html; charset=utf-8" . "\r\n";
 $headers .= "From: <$email>" . "\r\n";
 mail($email_to, "Message", $text, $headers);
 ?>
